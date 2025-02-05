24b90a47fd2effec543467786fc4ec7006a8db59
wRiqie <henriquegame512@outlook.com>
2025-02-05 16:03:57 -0300

    feat: testing cubit

---
+++ test/app/business_logic/cubits/motels/motels_cubit_test.dart
@@ -0,0 +1,82 @@
+import 'package:challenge_guia/app/business_logic/cubits/motels/motels_cubit.dart';
+import 'package:challenge_guia/app/business_logic/cubits/motels/motels_state.dart';
+import 'package:challenge_guia/app/data/models/default_response_model.dart';
+import 'package:challenge_guia/app/data/models/error_model.dart';
+import 'package:challenge_guia/app/data/models/motel_model.dart';
+import 'package:challenge_guia/app/data/models/paginable_model.dart';
+import 'package:challenge_guia/app/data/repositories/motels_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+// Mock do repositório
+class MockMotelsRepository extends Mock implements MotelsRepository {}
+
+void main() {
+  late MotelsCubit cubit;
+  late MockMotelsRepository mockRepository;
+
+  setUp(() {
+    mockRepository = MockMotelsRepository();
+    cubit = MotelsCubit(mockRepository);
+  });
+
+  tearDown(() {
+    cubit.close();
+  });
+
+  group('MotelsCubit', () {
+    final mockMotels = PaginableModel(
+      items: [
+        MotelModel(
+            name: 'Motel A',
+            logoUrl: '',
+            address: '',
+            distance: 0,
+            favoriteAmount: 0),
+        MotelModel(
+            name: 'Motel B',
+            logoUrl: '',
+            address: '',
+            distance: 0,
+            favoriteAmount: 0),
+      ],
+    );
+
+    final errorMock = ErrorModel('Erro ao buscar motéis');
+
+    blocTest<MotelsCubit, MotelsState>(
+      'Emite [loading, success] quando getAllPaginated() retorna dados com sucesso',
+      build: () {
+        when(() => mockRepository.getAllPaginated())
+            .thenAnswer((_) async => DefaultResponseModel(data: mockMotels));
+        return cubit;
+      },
+      act: (cubit) => cubit.getAllPaginated(),
+      expect: () => [
+        isA<MotelsState>()
+            .having((e) => e.status.isLoading, 'isLoading', isTrue),
+        isA<MotelsState>()
+            .having((e) => e.status.isSuccess, 'isSuccess', isTrue)
+            .having((e) => e.motels, 'Motels', equals(mockMotels))
+      ],
+    );
+
+    blocTest<MotelsCubit, MotelsState>(
+      'Emite [loading, error] quando getAllPaginated() falha',
+      build: () {
+        when(() => mockRepository.getAllPaginated())
+            .thenAnswer((_) async => DefaultResponseModel(error: errorMock));
+        return cubit;
+      },
+      act: (cubit) => cubit.getAllPaginated(),
+      expect: () => [
+        isA<MotelsState>()
+            .having((e) => e.status.isLoading, 'isLoading', isTrue),
+        isA<MotelsState>()
+            .having((e) => e.status.isError, 'isError', isTrue)
+            .having((e) => e.motels, 'Motels', isNull)
+      ],
+    );
+  });
+}
